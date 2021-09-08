e5accac64c925119314c2ef41cdfd6d91e480e46
jimin.joo <wnwlals22@naver.com>
2021-09-08 23:21:50 +0900

    test(Acceptance test): add Coupon test

---
+++ src/test/java/com/kakaopay/coupon/AcceptanceTest.java
@@ -0,0 +1,28 @@
+package com.kakaopay.coupon;
+
+
+import io.restassured.RestAssured;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.embedded.LocalServerPort;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class AcceptanceTest {
+
+    @LocalServerPort
+    int port;
+
+    @Autowired
+    DatabaseCleanUp databaseCleanUp;
+
+    @Before
+    public void setUp() {
+        RestAssured.port = port;
+        databaseCleanUp.execute();
+    }
+
+}
